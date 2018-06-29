5e5ffa355e9ff6cefad7d3df181c664674ca5422
Yury Garnov <hamta@yandex.ru>
2018-06-29 21:51:06 +0300

    copypast

--- vkbot_main.py
+++ vkbot_main.py
@@ -1,10 +1,17 @@
-from flask import Flask, request, json
-
+from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def processing():
-	print('hi')
-	return 'HI'
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-app.run()
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
